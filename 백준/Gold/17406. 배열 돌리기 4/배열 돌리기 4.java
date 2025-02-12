ef11e232ce7bc08a333337645481ec3c04bbffbc
전진호 <114588339+right5625@users.noreply.github.com>
2025-02-12 17:20:43 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 296 ms, Memory: 30588 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -2,17 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static BufferedReader br;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int N, M, K;
 	static int[][] A;
 	static int[][] rcs;
-	static ArrayList<int[]> seq = new ArrayList<>();
+	static ArrayList<int[]> seqList = new ArrayList<>();
+	static int[][] B;
 	static int result = Integer.MAX_VALUE;
-	static int[][] cur;
 	
 	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -34,12 +33,12 @@ public class Main {
 		
 		permutation(new ArrayList<>());
 		
-		for (int[] i: seq) {
-			rotate(i);
+		for (int[] seq: seqList) {
+			rotate(seq);
 			for (int r = 1; r < N + 1; r++) {
 				int sum = 0;
 				for (int c = 1; c < M + 1; c++) {
-					sum += cur[r][c];
+					sum += B[r][c];
 				}
 				result = Math.min(result, sum);
 			}
@@ -50,7 +49,7 @@ public class Main {
 	
 	static void permutation(ArrayList<Integer> list) {
 		if (list.size() == K) {
-			seq.add(list.stream().mapToInt(Integer::intValue).toArray());
+			seqList.add(list.stream().mapToInt(Integer::intValue).toArray());
 			return;
 		}
 		for (int i = 0; i < K; i++) {
@@ -62,35 +61,32 @@ public class Main {
 		}
 	}
 	
-	static void rotate(int[] sequence) {
-		cur = new int[N + 1][M + 1];
-		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < M + 1; j++) {
-				cur[i][j] = A[i][j];
+	static void rotate(int[] seq) {
+		B = new int[N + 1][M + 1];
+		for (int r = 1; r < N + 1; r++) {
+			for (int c = 1; c < M + 1; c++) {
+				B[r][c] = A[r][c];
 			}
 		}
-		for (int i: sequence) {
-			int r = rcs[i][0], c = rcs[i][1], s = rcs[i][2];
+		for (int n: seq) {
+			int r = rcs[n][0], c = rcs[n][1], s = rcs[n][2];
 			int minR = r - s, minC = c - s, maxR = r + s, maxC = c + s;
 			while (minR != maxR) {
-				int t = cur[minR][minC];
+				int t = B[minR][minC];
 				for (int k = minR + 1; k <= maxR; k++) {
-					cur[k - 1][minC] = cur[k][minC];
+					B[k - 1][minC] = B[k][minC];
 				}
 				for (int k = minC + 1; k <= maxC; k++) {
-					cur[maxR][k - 1] = cur[maxR][k];
+					B[maxR][k - 1] = B[maxR][k];
 				}
 				for (int k = maxR - 1; k >= minR; k--) {
-					cur[k + 1][maxC] = cur[k][maxC];
+					B[k + 1][maxC] = B[k][maxC];
 				}
-				for (int k = maxC - 1; k > minC; k--) {
-					cur[minR][k + 1] = cur[minR][k];
+				for (int k = maxC - 1; k >= minC; k--) {
+					B[minR][k + 1] = B[minR][k];
 				}
-				cur[minR][minC + 1] = t;
-				minR++;
-				minC++;
-				maxR--;
-				maxC--;
+				B[minR][minC + 1] = t;
+				minR++; minC++; maxR--; maxC--;
 			}
 		}
 	}
