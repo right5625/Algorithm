51c2f0232545c4d80ac6c2a17287346de4945218
전진호 <114588339+right5625@users.noreply.github.com>
2025-02-22 11:45:19 +0900

    [Bronze III] Title: Огни светофора, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29412. Огни светофора/Огни светофора.py
+++ 백준/Bronze/29412. Огни светофора/Огни светофора.py
@@ -1,10 +1,9 @@
 def calculate(g, gb, y, r, ry):
     S = g + gb + y + r + ry
-    full = T // S
+    red = T // S * (r + ry)
+    yellow = T // S * (y + ry)
+    green = T // S * (g + gb // 2)
     rem = T % S
-    red = full * (r + ry)
-    yellow = full * (y + ry)
-    green = full * (g + gb // 2)
     time = 0
     if time + g <= rem:
         green += g
@@ -38,6 +37,7 @@ def calculate(g, gb, y, r, ry):
         red += rem - time
         yellow += rem - time
     return red, yellow, green
+
 g, gb, y, r, ry = map(int, input().split())
 T = int(input())
 print(*calculate(g, gb, y, r, ry))
