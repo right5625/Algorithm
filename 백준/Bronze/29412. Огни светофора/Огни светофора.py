71b15b8c64eb27762a2d727a2d3b74c62efce3e9
전진호 <114588339+right5625@users.noreply.github.com>
2025-02-22 11:43:04 +0900

    [Bronze III] Title: Огни светофора, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29412. Огни светофора/Огни светофора.py
+++ 백준/Bronze/29412. Огни светофора/Огни светофора.py
@@ -6,39 +6,38 @@ def calculate(g, gb, y, r, ry):
     yellow = full * (y + ry)
     green = full * (g + gb // 2)
     time = 0
-    if rem > 0:
-        if time + g <= rem:
-            green += g
-            time += g
-        else:
-            green += rem - time
-            return int(red), int(yellow), int(green)
-        if time + gb <= rem:
-            green += gb // 2
-            time += gb
-        else:
-            green += (rem - time) // 2
-            return int(red), int(yellow), int(green)
-        if time + y <= rem:
-            yellow += y
-            time += y
-        else:
-            yellow += rem - time
-            return int(red), int(yellow), int(green)
-        if time + r <= rem:
-            red += r
-            time += r
-        else:
-            red += rem - time
-            return int(red), int(yellow), int(green)
-        if time + ry <= rem:
-            red += ry
-            yellow += ry
-            time += ry
-        else:
-            red += rem - time
-            yellow += rem - time
-    return int(red), int(yellow), int(green)
+    if time + g <= rem:
+        green += g
+        time += g
+    else:
+        green += rem - time
+        return red, yellow, green
+    if time + gb <= rem:
+        green += gb // 2
+        time += gb
+    else:
+        green += (rem - time) // 2
+        return red, yellow, green
+    if time + y <= rem:
+        yellow += y
+        time += y
+    else:
+        yellow += rem - time
+        return red, yellow, green
+    if time + r <= rem:
+        red += r
+        time += r
+    else:
+        red += rem - time
+        return red, yellow, green
+    if time + ry <= rem:
+        red += ry
+        yellow += ry
+        time += ry
+    else:
+        red += rem - time
+        yellow += rem - time
+    return red, yellow, green
 g, gb, y, r, ry = map(int, input().split())
 T = int(input())
 print(*calculate(g, gb, y, r, ry))
