ecaa8bb7ee0a872dea6d4049136a2df3b205488d
전진호 <114588339+right5625@users.noreply.github.com>
2025-02-25 10:08:57 +0900

    [Silver V] Title: 집합, Time: 1972 ms, Memory: 300456 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringTokenizer st;
+    	Set<Integer> set = new HashSet<>();
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+        	st = new StringTokenizer(br.readLine(), " ");
+        	String s = st.nextToken();
+        	if (s.equals("add")) {
+        		set.add(Integer.parseInt(st.nextToken()));
+        	} else if (s.equals("remove")) {
+        		set.remove(Integer.parseInt(st.nextToken()));
+        	} else if (s.equals("check")) {
+        		bw.append(set.contains(Integer.parseInt(st.nextToken())) ? "1\n" : "0\n");
+        	} else if (s.equals("toggle")) {
+        		int x = Integer.parseInt(st.nextToken());
+        		if (!set.remove(x)) {
+        			set.add(x);
+        		}
+        	} else if (s.equals("all")) {
+        		set = new HashSet<>();
+        		for (int j = 1; j <= 20; j++) {
+        			set.add(j);
+        		}
+        	} else {
+        		set.clear();
+        	}
+        }
+        bw.flush();
+    }
+}
