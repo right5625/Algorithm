1920d758351d5be3c7329c4f1d2eb50edb969155
전진호 <114588339+right5625@users.noreply.github.com>
2025-03-13 10:39:57 +0900

    [Bronze II] Title: Õhne vanaraamatupood, Time: 504 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7131. Õhne vanaraamatupood/Õhne vanaraamatupood.py
+++ 백준/Bronze/7131. Õhne vanaraamatupood/Õhne vanaraamatupood.py
@@ -1,7 +1,3 @@
-def round2(n):
-    n *= 100
-    return (int(n) + 1) / 100 if n - int(n) >= 0.5 else int(n) / 100
-
 N, P, T = map(lambda x: int(x) if x.isdigit() else float(x), input().split())
 SIM = [list(map(lambda x: int(x) if x.isdigit() else float(x), input().split())) for _ in range(N)]
 res = [0] * N
@@ -14,6 +10,6 @@ for i in range(1, T):
             cnt += 1
     for j in range(N):
         if i >= SIM[j][0] and (i - SIM[j][0]) % SIM[j][1] == 0:
-            res[j] = round2(s / cnt * (1 + SIM[j][2]))
+            res[j] = round(s / cnt * (1 + SIM[j][2]), 2)
 for i in range(N):
     print(f'{res[i]:.2f}')
