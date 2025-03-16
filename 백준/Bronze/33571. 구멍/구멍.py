aa77da845bf8cd672347a05d61b5bbcfa2b96e1e
전진호 <114588339+right5625@users.noreply.github.com>
2025-03-16 10:35:06 +0900

    [Bronze IV] Title: 구멍, Time: 116 ms, Memory: 37140 KB -BaekjoonHub

---
+++ 백준/Bronze/33571. 구멍/구멍.py
@@ -0,0 +1,4 @@
+import string
+
+dic = {i: j for i, j in zip(string.ascii_letters + '@ ', [1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])}
+print(sum(dic[i] for i in input()))
