9fe13ff60be3202a535e080423e476dad977d8fe
전진호 <114588339+right5625@users.noreply.github.com>
2025-06-01 16:38:52 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 964 ms, Memory: 42220 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,83 +1,51 @@
+late = (0.05, 0.01, 0.07, 0.1, 0.02, 0.01, 0.07, 0.1, 0.02)
+dr = ((0, -1, -1, -1, -2, 1, 1, 1, 2), (3, 0, 1, 2, 1, 0, 1, 2, 1), (0, -1, -1, -1, -2, 1, 1, 1, 2), (-3, 0, -1, -2, -1, 0, -1, -2, -1))
+dc = ((-3, 0, -1, -2, -1, 0, -1, -2, -1), (0, -1, -1, -1, -2, 1, 1, 1, 2), (3, 0, 1, 2, 1, 0, 1, 2, 1), (0, -1, -1, -1, -2, 1, 1, 1, 2))
+
+def tornado(r, c, ar, ac):
+    global result
+    n = A[r][c]
+    for i in range(9):
+        mr, mc = curR + dr[curD][i], curC + dc[curD][i]
+        m = int(n * late[i])
+        if 0 <= mr < N and 0 <= mc < N:
+            A[mr][mc] += m
+        else:
+            result += m
+        A[r][c] -= m
+    if 0 <= ar < N and 0 <= ac < N:
+        A[ar][ac] += A[r][c]
+    else:
+        result += A[r][c]
+    A[r][c] = 0
+
 N = int(input())
 A = [list(map(int, input().split())) for _ in range(N)]
+curD = 0
 curR = curC = N // 2
 left, right = curC - 1, curC + 1
-d = 0
-late = (0.05, 0.01, 0.07, 0.1, 0.02, 0.01, 0.07, 0.1, 0.02)
-dr0, dc0 = (0, -1, -1, -1, -2, 1, 1, 1, 2), (-3, 0, -1, -2, -1, 0, -1, -2, -1)
-dr1, dc1 = (3, 0, 1, 2, 1, 0, 1, 2, 1), (0, -1, -1, -1, -2, 1, 1, 1, 2)
-dr2, dc2 = (0, -1, -1, -1, -2, 1, 1, 1, 2), (3, 0, 1, 2, 1, 0, 1, 2, 1)
-dr3, dc3 = (-3, 0, -1, -2, -1, 0, -1, -2, -1), (0, -1, -1, -1, -2, 1, 1, 1, 2)
 result = 0
 while curR or curC:
-    if d == 0:
-        n = A[curR][curC - 1]
-        for i in range(9):
-            mr, mc = curR + dr0[i], curC + dc0[i]
-            if 0 <= mr < N and 0 <= mc < N:
-                A[mr][mc] += int(n * late[i])
-            else:
-                result += int(n * late[i])
-            A[curR][curC - 1] -= int(n * late[i])
-        if curC - 2 >= 0:
-            A[curR][curC - 2] += A[curR][curC - 1]
-        else:
-            result += A[curR][curC - 1]
-        A[curR][curC - 1] = 0
+    if curD == 0:
+        tornado(curR, curC - 1, curR, curC - 2)
         curC -= 1
         if curC == left:
-            d = 1
-    elif d == 1:
-        n = A[curR + 1][curC]
-        for i in range(9):
-            mr, mc = curR + dr1[i], curC + dc1[i]
-            if 0 <= mr < N and 0 <= mc < N:
-                A[mr][mc] += int(n * late[i])
-            else:
-                result += int(n * late[i])
-            A[curR + 1][curC] -= int(n * late[i])
-        if curR + 2 < N:
-            A[curR + 2][curC] += A[curR + 1][curC]
-        else:
-            result += A[curR + 1][curC]
-        A[curR + 1][curC] = 0
+            curD = 1
+    elif curD == 1:
+        tornado(curR + 1, curC, curR + 2, curC)
         curR += 1
         if curR == right:
-            d = 2
-    elif d == 2:
-        n = A[curR][curC + 1]
-        for i in range(9):
-            mr, mc = curR + dr2[i], curC + dc2[i]
-            if 0 <= mr < N and 0 <= mc < N:
-                A[mr][mc] += int(n * late[i])
-            else:
-                result += int(n * late[i])
-            A[curR][curC + 1] -= int(n * late[i])
-        if curC + 2 < N:
-            A[curR][curC + 2] += A[curR][curC + 1]
-        else:
-            result += A[curR][curC + 1]
-        A[curR][curC + 1] = 0
+            curD = 2
+    elif curD == 2:
+        tornado(curR, curC + 1, curR, curC + 2)
         curC += 1
         if curC == right:
-            d = 3
+            curD = 3
     else:
-        n = A[curR - 1][curC]
-        for i in range(9):
-            mr, mc = curR + dr3[i], curC + dc3[i]
-            if 0 <= mr < N and 0 <= mc < N:
-                A[mr][mc] += int(n * late[i])
-            else:
-                result += int(n * late[i])
-            A[curR - 1][curC] -= int(n * late[i])
-        if curR - 2 >= 0:
-            A[curR - 2][curC] += A[curR - 1][curC]
-        else:
-            result += A[curR - 1][curC]
-        A[curR - 1][curC] = 0
+        tornado(curR - 1, curC, curR - 2, curC)
         curR -= 1
         if curR == left:
-            d = 0
+            curD = 0
             left -= 1
             right += 1
 print(result)
