8cfe5745ea48d68284d7911ed8d0e48bc8063e72
전진호 <114588339+right5625@users.noreply.github.com>
2025-06-03 13:41:53 +0900

    [Gold III] Title: 별자리 만들기, Time: 56 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -0,0 +1,24 @@
+import heapq
+
+n = int(input())
+graph = [[] for _ in range(n)]
+coordinates = [list(map(float, input().split())) for _ in range(n)]
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        dist = ((coordinates[i][0] - coordinates[j][0]) ** 2 + (coordinates[i][1] - coordinates[j][1]) ** 2) ** 0.5
+        graph[i].append((dist, j))
+        graph[j].append((dist, i))
+heap = []
+for i in graph[0]:
+    heapq.heappush(heap, i)
+vst = set()
+vst.add(0)
+result = 0
+while heap:
+    d, n = heapq.heappop(heap)
+    if n not in vst:
+        for i in graph[n]:
+            heapq.heappush(heap, i)
+        vst.add(n)
+        result += d
+print(result)
