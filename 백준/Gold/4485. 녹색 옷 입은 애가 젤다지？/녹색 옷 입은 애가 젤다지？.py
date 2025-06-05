2478cb16f9677abf4fd4b11ad37122986f2ae067
전진호 <114588339+right5625@users.noreply.github.com>
2025-06-05 11:10:09 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 176 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -9,17 +9,17 @@ while True:
     A = [list(map(int, input().split())) for _ in range(N)]
     dist = [[float('inf') for _ in range(N)] for _ in range(N)]
     dist[0][0] = A[0][0]
-    vst = set()
-    vst.add((0, 0))
+    vst = [[False for _ in range(N)] for _ in range(N)]
+    vst[0][0] = True
     heap = [(dist[0][0], (0, 0))]
     while heap:
         d, rc = heapq.heappop(heap)
         for i in range(4):
             mr, mc = rc[0] + dr[i], rc[1] + dc[i]
-            if 0 <= mr < N and 0 <= mc < N and (mr, mc) not in vst:
+            if 0 <= mr < N and 0 <= mc < N and not vst[mr][mc]:
                 if dist[mr][mc] > d + A[mr][mc]:
                     dist[mr][mc] = d + A[mr][mc]
                     heapq.heappush(heap, (dist[mr][mc], (mr, mc)))
-                vst.add((mr, mc))
+                vst[mr][mc] = True
     print(f'Problem {num}: {dist[N - 1][N - 1]}')
     num += 1
