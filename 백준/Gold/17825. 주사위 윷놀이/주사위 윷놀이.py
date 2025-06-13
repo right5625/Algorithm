c77faa9eea5380d7637a66a80b515bd786064b9a
전진호 <114588339+right5625@users.noreply.github.com>
2025-06-13 15:13:56 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 440 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.py
@@ -1,29 +1,24 @@
-from copy import deepcopy
-
 board = {'0': ['2'], '2': ['4'], '4': ['6'], '6': ['8'], '8': ['10b'], '10b': ['12', '13b'], '12': ['14'], '14': ['16'], '16': ['18'], '18': ['20b'], '13b': ['16b'], '16b': ['19b'], '19b': ['25'], '20b': ['22', '22b'], '22': ['24'], '24': ['26'], '26': ['28'], '28': ['30b'], '22b': ['24b'], '24b': ['25'], '30b': ['32', '28b'], '32': ['34'], '34': ['36'], '36': ['38'], '38': ['40'], '28b': ['27b'], '27b': ['26b'], '26b': ['25'], '25': ['30'], '30': ['35'], '35': ['40'], '40': ['end']}
 def play(cur_pos_horses, score, turn):
     if turn == 10:
         global result
         result = max(result, score)
         return
-    next_pos_horses = deepcopy(cur_pos_horses)
     for i in range(4):
         if cur_pos_horses[i] == 'end':
             continue
-        if cur_pos_horses[i][-1] == 'b' and len(board[cur_pos_horses[i]]) == 2:
-            cur = board[cur_pos_horses[i]][1]
-        else:
-            cur = board[cur_pos_horses[i]][0]
+        cur = board[cur_pos_horses[i]][1] if cur_pos_horses[i][-1] == 'b' and len(board[cur_pos_horses[i]]) == 2 else board[cur_pos_horses[i]][0]
         for _ in range(dice[turn] - 1):
             if cur == 'end':
                 break
             cur = board[cur][0]
-        next_pos_horses[i] = cur
-        if cur == 'end':
-            play(next_pos_horses, score, turn + 1)
-        elif cur not in cur_pos_horses:
-            play(next_pos_horses, score + int(next_pos_horses[i][:-1] if next_pos_horses[i][-1] == 'b' else next_pos_horses[i]), turn + 1)
-        next_pos_horses[i] = cur_pos_horses[i]
+        if cur != 'end' and cur in cur_pos_horses:
+            continue
+        origin = cur_pos_horses[i]
+        cur_pos_horses[i] = cur
+        add_score = 0 if cur == 'end' else int(cur[:-1] if cur[-1] == 'b' else cur)
+        play(cur_pos_horses, score + add_score, turn + 1)
+        cur_pos_horses[i] = origin
 
 dice = list(map(int, input().split()))
 result = 0
