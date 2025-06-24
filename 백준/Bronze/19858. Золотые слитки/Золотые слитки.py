8fa58794cdb728357aabb5baae3b33d1b7518e42
전진호 <114588339+right5625@users.noreply.github.com>
2025-06-24 11:28:46 +0900

    [Bronze II] Title: Золотые слитки, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19858. Золотые слитки/Золотые слитки.py
+++ 백준/Bronze/19858. Золотые слитки/Золотые слитки.py
@@ -1,18 +1,15 @@
 x1, x2, x3 = map(int, input().split())
-if max(x1, x2, x3) - sum([x1, x2, x3]) == -max(x1, x2, x3):
+if max(x1, x2, x3) * 2 == sum([x1, x2, x3]):
     print(0)
 else:
-    result1 = result2 = 0
+    flag = False
     for i in range(3):
         x = [x1, x2, x3]
         x = [x.pop(i)] + x
         if abs(x[1] - x[2]) <= x[0] and abs(x[1] - x[2]) % 2 == x[0] % 2:
-            rem = x[0]
-            result1 = abs(x[1] - x[2])
-            rem -= abs(x[1] - x[2])
-            result1 += rem // 2
-            result2 += rem // 2
-            print(f'{i + 1}\n{result1} {result2}')
+            rem = x[0] - abs(x[1] - x[2])
+            print(f'{i + 1}\n{abs(x[1] - x[2]) + rem // 2} {rem // 2}')
+            flag = True
             break
-    if result1 == result2 == 0:
+    if not flag:
         print(-1)
