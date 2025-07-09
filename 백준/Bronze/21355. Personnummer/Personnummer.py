b6a8d810adc0e22d92b183d2d32c80a6ff84866b
전진호 <114588339+right5625@users.noreply.github.com>
2025-07-09 22:39:09 +0900

    [Bronze II] Title: Personnummer, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/21355. Personnummer/Personnummer.py
+++ 백준/Bronze/21355. Personnummer/Personnummer.py
@@ -1,5 +1,8 @@
 S = input()
 if '-' in S:
-    print('20' + ''.join(S.split('-')) if int(S[:2]) < 20 else '19' + ''.join(S.split('-')))
+    A = 20
+    sep = '-'
 else:
-    print('19' + ''.join(S.split('+')) if int(S[:2]) < 20 else '18' + ''.join(S.split('+')))
+    A = 19
+    sep = '+'
+print(str(A if int(S[:2]) < 20 else A - 1) + ''.join(S.split(sep)))
