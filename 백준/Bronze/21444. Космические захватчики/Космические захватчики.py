29a08669942e5de28c3b85b6694327c3c36958e5
전진호 <114588339+right5625@users.noreply.github.com>
2025-07-11 21:21:43 +0900

    [Bronze II] Title: Космические захватчики, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/21444. Космические захватчики/Космические захватчики.py
+++ 백준/Bronze/21444. Космические захватчики/Космические захватчики.py
@@ -1,8 +1,4 @@
 from math import ceil
 
 n, p = map(int, input().split())
-a = list(map(int, input().split()))
-if ceil(n / 2) < p:
-    print(n - p + n - 1 + sum(a))
-else:
-    print(p - 1 + n - 1 + sum(a))
+print((n * 2 - p - 1 if ceil(n / 2) < p else n + p - 2) + sum(list(map(int, input().split()))))
