dd7ba336070a93724123040784f263dd948b92ac
전진호 <114588339+right5625@users.noreply.github.com>
2025-08-04 23:07:36 +0900

    [Bronze II] Title: Звезды на погонах, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19772. Звезды на погонах/Звезды на погонах.py
+++ 백준/Bronze/19772. Звезды на погонах/Звезды на погонах.py
@@ -1,2 +1,2 @@
 a, b, c, d, e = map(int, input().split())
-print(f'{max(0, d - max(1, c - b)) + max(0, c - a - e)} {b - a + 1 - (1 if d == e else 2)}')
+print(max(0, d - max(1, c - b)) + max(0, c - a - e), b - a + 1 - (1 if d == e else 2))
