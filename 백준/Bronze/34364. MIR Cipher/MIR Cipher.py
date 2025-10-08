067c0e06b65953c4636921a1b9281799557c2f6d
전진호 <114588339+right5625@users.noreply.github.com>
2025-10-08 11:22:43 +0900

    [Bronze III] Title: MIR Cipher, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34364. MIR Cipher/MIR Cipher.py
@@ -0,0 +1,7 @@
+N, S = input().split()
+move = 1
+result = ''
+for i in S:
+    result += chr(ord(i) + move) if chr(ord(i) + move) <= 'Z' else chr(ord(i) + move - 26)
+    move = (move * 2) % 26
+print(result)
