d15ad012eeac97284d22a377383350fc78161aba
전진호 <114588339+right5625@users.noreply.github.com>
2025-11-23 17:10:35 +0900

    [Gold III] Title: MST 게임, Time: 548 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/16202. MST 게임/MST 게임.py
+++ 백준/Gold/16202. MST 게임/MST 게임.py
@@ -16,30 +16,27 @@ def union(v1, v2):
         parent[a] = b
 
 N, M, K = map(int, input().split())
-edges = [(0, 0, 0)]
+edges = [(0, 0)]
 for i in range(1, M + 1):
     x, y = map(int, input().split())
-    edges.append((i, x, y))
+    edges.append((x, y))
 impossible_edges = [False] * (M + 1)
 result = [0] * K
 for i in range(K):
     parent = [j for j in range(N + 1)]
     min_weight = float('inf')
-    min_weight_edge = -1
     use = score = 0
-    for j in range(1, M + 1):
-        if impossible_edges[j]:
+    for weight in range(1, M + 1):
+        if impossible_edges[weight]:
             continue
-        weight, vertex1, vertex2 = edges[j]
+        vertex1, vertex2 = edges[weight]
         if find(vertex1) != find(vertex2):
             union(vertex1, vertex2)
             score += weight
             use += 1
-            if weight < min_weight:
-                min_weight = weight
-                min_weight_edge = j
+            min_weight = min(min_weight, weight)
     if use < N - 1:
         break
+    impossible_edges[min_weight] = True
     result[i] = score
-    impossible_edges[min_weight_edge] = True
 print(*result)
