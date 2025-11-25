3ff4b9e15ca8ab0a745801d28b5dbc2c270957c5
전진호 <114588339+right5625@users.noreply.github.com>
2025-11-26 01:22:22 +0900

    [Gold III] Title: 스티커 붙이기, Time: 572 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.py
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.py
@@ -1,9 +1,23 @@
+def attach_sticker():
+    for r in range(N):
+        for c in range(M):
+            if notebook[r][c] == 0:
+                cnt = 0
+                for sticker_r, sticker_c in sticker_pos:
+                    mr, mc = r + sticker_r - sticker_start_r, c + sticker_c - sticker_start_c
+                    if 0 <= mr < N and 0 <= mc < M and notebook[mr][mc] == 0:
+                        cnt += 1
+                if cnt == len(sticker_pos):
+                    for sticker_r, sticker_c in sticker_pos:
+                        notebook[r + sticker_r - sticker_start_r][c + sticker_c - sticker_start_c] = 1
+                    return True
+    return False
+
 N, M, K = map(int, input().split())
 notebook = [[0 for _ in range(M)] for _ in range(N)]
 for _ in range(K):
     R, C = map(int, input().split())
     sticker = [list(map(int, input().split())) for _ in range(R)]
-    attach = False
     for _ in range(4):
         sticker_pos = []
         for sticker_r in range(R):
@@ -11,21 +25,7 @@ for _ in range(K):
                 if sticker[sticker_r][sticker_c] == 1:
                     sticker_pos.append([sticker_r, sticker_c])
         sticker_start_r, sticker_start_c = sticker_pos[0]
-        for r in range(N):
-            for c in range(M):
-                if notebook[r][c] == 0:
-                    cnt = 0
-                    for sticker_r, sticker_c in sticker_pos:
-                        mr, mc = r + sticker_r - sticker_start_r, c + sticker_c - sticker_start_c
-                        if 0 <= mr < N and 0 <= mc < M and notebook[mr][mc] == 0:
-                            cnt += 1
-                    if cnt == len(sticker_pos):
-                        for sticker_r, sticker_c in sticker_pos:
-                            notebook[r + sticker_r - sticker_start_r][c + sticker_c - sticker_start_c] = 1
-                        attach = True
-                        break
-            if attach:
-                break
+        attach = attach_sticker()
         if attach:
             break
         sticker = [[0 for _ in range(R)] for _ in range(C)]
