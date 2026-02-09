46fefadba381a17ca8ebc8d06c916a706236e531
전진호 <114588339+right5625@users.noreply.github.com>
2026-02-09 17:55:00 +0900

    [Bronze II] Title: 사각형 면적, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22341. 사각형 면적/사각형 면적.py
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.py
@@ -1,9 +1,12 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
 N, C = map(int, input().split())
 A = B = N
 for _ in range(C):
     X, Y = map(int, input().split())
     if X < A and Y < B:
-        if X * B > Y * A:
+        if X * B >= Y * A:
             A = X
         else:
             B = Y
