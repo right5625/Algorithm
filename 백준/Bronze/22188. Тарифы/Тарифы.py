ae70f7f57ef7153fda73d6a99af83b5c2ce73dc5
전진호 <114588339+right5625@users.noreply.github.com>
2026-03-11 21:26:04 +0900

    [Bronze II] Title: Тарифы, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/22188. Тарифы/Тарифы.py
+++ 백준/Bronze/22188. Тарифы/Тарифы.py
@@ -1,8 +1,7 @@
 n, m = map(int, input().split())
 ctp = [list(map(int, input().split())) for _ in range(n)]
 d = list(map(int, input().split()))
-min_cost = float('inf')
-result = 1
+min_cost, result = float('inf'), 1
 for i, (c, t, p) in enumerate(ctp, 1):
     total = c * 100 + sum(((v + t - 1) // t) * p for v in d if v >= t)
     if total < min_cost:
