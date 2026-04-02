e9f53d673bb035566e664de4493a72f07dc4ceb9
전진호 <114588339+right5625@users.noreply.github.com>
2026-04-02 14:02:38 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,8 @@
+select A.ITEM_ID, A.ITEM_NAME, A.RARITY
+from ITEM_INFO A, (
+    select B.ITEM_ID
+    from ITEM_INFO A, ITEM_TREE B
+    where A.ITEM_ID = B.PARENT_ITEM_ID and A.RARITY = 'RARE'
+) B
+where A.ITEM_ID = B.ITEM_ID
+order by ITEM_ID desc;
