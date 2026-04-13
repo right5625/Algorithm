0401f5e9ba02b3113a9859b981661be0743c19ec
전진호 <114588339+right5625@users.noreply.github.com>
2026-04-13 14:30:48 +0900

    [Bronze II] Title: Queue skipping (Easy), Time: 176 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27659. Queue skipping （Easy）/Queue skipping （Easy）.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    input()
+    n, e = map(int, input().split())
+    lst = [i for i in range(1, n + 1)]
+    for _ in range(e):
+        m = int(input())
+        lst = [lst.pop(lst.index(m))] + lst
+    print(lst[-1])
